ba1b063e71bab3fee01cf1675c8127ae2bd3baf9
soomin0209 <soomin02094@gmail.com>
2026-01-30 20:57:39 +0900

    feat: Lv 1 API 요청 INFO 로그 추가

--- src/main/java/com/cloudarchitecture/member/controller/MemberController.java
+++ src/main/java/com/cloudarchitecture/member/controller/MemberController.java
@@ -4,12 +4,13 @@ import com.cloudarchitecture.member.dto.GetMemberResponse;
 import com.cloudarchitecture.member.dto.SaveMemberRequest;
 import com.cloudarchitecture.member.dto.SaveMemberResponse;
 import com.cloudarchitecture.member.service.MemberService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
@@ -19,11 +20,13 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity<SaveMemberResponse> saveMember(@RequestBody SaveMemberRequest request) {
+        log.info("[API-LOG] 팀원 저장 요청: name={}, age={}, mbti={}", request.getName(), request.getAge(), request.getMbti());
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.saveMember(request));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long id) {
+        log.info("[API-LOG] 팀원 조회 요청: id={}", id);
         return ResponseEntity.status(HttpStatus.OK).body(memberService.getMember(id));
     }
 }
